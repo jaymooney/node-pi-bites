07e3c187fc9616baa2fb31bdafcb86f01bf4c2fd
Jay Mooney <jay.mooney@gmail.com>
2015-05-29 15:09:36 -0700

    button -> switch

--- ledtest.js
+++ ledtest.js
@@ -2,23 +2,36 @@ var Gpio = require("onoff").Gpio;
 
 var pins = {}
 
-pins.button = new Gpio(25, "in", "both");
+pins.switch = new Gpio(25, "in", "both");
 pins.red = new Gpio(18, "high");
 pins.green = new Gpio(22, "high");
 pins.blue = new Gpio(23, "high");
 
 var color = 0;
+var interval;
 
-pins.button.watch(function(err, value) {
+pins.switch.watch(function(err, value) {
 	if (err) throw err;
-	if (!value) return;
+	if (value) {
+		lights();
+		interval = setInterval(lights, 1000)
+	} else {
+		clearInterval(interval);
+		interval = undefined;
+		pins.red.write(1);
+		pins.green.write(1);
+		pins.blue.write(1);
+	}
+});
+
+function lights() {
 	color = (color + 1 ) % 3
 	console.log("color" + color);
 	// really? passing true or false doesn't work
 	pins.red.write(color === 0 ? 0 : 1);
 	pins.green.write(color === 1 ? 0 : 1);
 	pins.blue.write(color === 2 ? 0 : 1);
-});
+}
 
 function exit() {
 	for (var k in pins) {
